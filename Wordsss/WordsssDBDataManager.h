53cf987dcfb6fab1306142ade51bbe35b6121f49
Kelvin Ren <renkelvin@gmail.com>
2011-12-11 16:16:11 +0800

    ~

---
+++ Wordsss/WordsssDBDataManager.h
@@ -0,0 +1,20 @@
+//
+//  WordsssDBDataManager.h
+//  Wordsss
+//
+//  Created by Kelvin Ren on 12/11/11.
+//  Copyright (c) 2011 Ren Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WordsssDBDataManager : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
